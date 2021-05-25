2f62216b54894804cdeaca13a76d8ccfb85b73d8
alonely0 <4lon3ly0@protonmail.com>
2021-05-26 01:18:05 +0200

    Add Polybar dotfiles

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Kill polybar
+killall -q polybar
+
+# Await polybar processes to finish
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# execute Polybar in all screens
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "Polybar launched..."
